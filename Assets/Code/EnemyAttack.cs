c9d67e46f21411e927b039bf81cd7e008f82f641
onnijuola <onni.juola@student.careeria.fi>
2024-05-04 20:47:31 +0300

    first commit

---
+++ Assets/Code/EnemyAttack.cs
@@ -0,0 +1,19 @@
+/*Attach this script to the enemy's weapon or collider. When the weapon collides with a player (assuming the player has a HealthSystem component),
+ * it will cause damage to the player's health. Make sure the colliders are set up appropriately and tagged if needed for collision detection.^*/
+
+
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public int damageAmount = 10;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        HealthSystem healthSystem = collision.gameObject.GetComponent<HealthSystem>();
+        if (healthSystem != null)
+        {
+            healthSystem.TakeDamage(damageAmount);
+        }
+    }
+}
